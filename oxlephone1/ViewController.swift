d1f252dcc03e3409741e89abbf1666fffd5df1f3
mohamed <mohamednaeem747@gmail.com>
2018-10-08 02:28:29 -0700

    Initial Commit

---
+++ oxlephone1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  oxlephone1
+//
+//  Created by Na3em on 10/8/18.
+//  Copyright © 2018 Na3em. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
